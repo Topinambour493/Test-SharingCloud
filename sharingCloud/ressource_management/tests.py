56b9bc948290554254e1c9b9a6903d8a4a940571
corentin jezierski <corentin.jezierski@gmail.com>
2022-04-20 00:53:39 +0200

    ajout des tests unitaires pour l'application ressources

--- sharingCloud/ressource_management/tests.py
+++ sharingCloud/ressource_management/tests.py
@@ -1,24 +1,65 @@
 from itertools import tee
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 from .models import Ressource
 
-class IndexViewTests(TestCase):
+class General():
+
+    def connexion_user(self):
+        self.client = Client()
+        user = User.objects.create(username='testuser',password="password")
+        self.client.force_login(user, backend=None)
+
+    def connexion_superuser(self):
+        self.client = Client()
+        user = User.objects.create_superuser(username='testuser',password="password")
+        self.client.force_login(user, backend=None)
+
+
+class IndexViewTests(General,TestCase):
+
+    def test_not_connected(self):
+        """
+        If a user is not connected, a user is rediriged
+        """
+        ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
+        response = self.client.get(reverse('ressource_management:index'))
+        self.assertEqual(response.status_code, 302)
+    
+    def test_connected_user(self):
+        """
+        If a user is not superuser , he has access to the page
+        """
+        self.connexion_user()
+        ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
+        response = self.client.get(reverse('ressource_management:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_connected_superuser(self):
+        """
+        If a user is superuser, he has access to the page
+        """
+        self.connexion_user()
+        ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
+        response = self.client.get(reverse('ressource_management:index'))
+        self.assertEqual(response.status_code, 200)
 
     def test_no_ressources(self):
         """
         If no ressources exist, an appropriate message is displayed.
         """
+        self.connexion_user()
         response = self.client.get(reverse('ressource_management:index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No ressource_management are available.")
         self.assertQuerysetEqual(response.context['ressource_list'], [])
 
     def test_past_ressource(self):
         """
         The ressources index page may display one ressource.
         """
+        self.connexion_user()
         ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
         response = self.client.get(reverse('ressource_management:index'))
         self.assertQuerysetEqual(response.context['ressource_list'],[ressource])
@@ -27,18 +68,46 @@ class IndexViewTests(TestCase):
         """
         The ressources index page may display multiple ressources.
         """
+        self.connexion_user()
         ressource1 = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A, rez de chaussé", people_capacity=200)
         ressource2 = Ressource.objects.create(type="salle de réunion", label="B7", localization="Batiment B, 1er étage", people_capacity=30)
         response = self.client.get(reverse('ressource_management:index'))
         self.assertQuerysetEqual(response.context['ressource_list'],[ressource1, ressource2],ordered=False)
 
 
-class DetailViewTest(TestCase):
+class DetailViewTest(General,TestCase):
+
+    def test_not_connected(self):
+        """
+        If a user is not connected, a user is rediriged
+        """
+        ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
+        response = self.client.get(reverse('ressource_management:detail',args=[1]))
+        self.assertEqual(response.status_code, 302)
+    
+    def test_connected_user(self):
+        """
+        If a user is not superuser , he has access to the page
+        """
+        self.connexion_user()
+        ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
+        response = self.client.get(reverse('ressource_management:detail',args=[1]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_connected_superuser(self):
+        """
+        If a user is superuser, he has access to the page
+        """
+        self.connexion_superuser()
+        ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
+        response = self.client.get(reverse('ressource_management:detail',args=[1]))
+        self.assertEqual(response.status_code, 200)
 
     def test_no_ressource(self):
         """
         If no ressources exist, an appropriate message is displayed.
         """
+        self.connexion_user()
         response = self.client.get(reverse('ressource_management:detail',args=[1]))
         self.assertEqual(response.status_code, 404)
 
@@ -46,17 +115,45 @@ class DetailViewTest(TestCase):
         """
         The ressource detail page may display ressource.
         """
+        self.connexion_user()
         ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
         response = self.client.get(reverse('ressource_management:detail',args=[1]))
         self.assertEqual(response.context['ressource'],ressource)
 
 
-class UpdateViewTest(TestCase):
+class UpdateViewTest(General,TestCase):
+
+    def test_not_connected(self):
+        """
+        If a user is not connected, a user is rediriged
+        """
+        ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
+        response = self.client.get(reverse('ressource_management:update',args=[1]))
+        self.assertEqual(response.status_code, 302)
+    
+    def test_connected_user(self):
+        """
+        If a user is not superuser , a user is blocked
+        """
+        self.connexion_user()
+        ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
+        response = self.client.get(reverse('ressource_management:update',args=[1]))
+        self.assertEqual(response.status_code, 403)
+
+    def test_connected_superuser(self):
+        """
+        If a user is superuser, he has access to the page
+        """
+        self.connexion_superuser()
+        ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
+        response = self.client.get(reverse('ressource_management:update',args=[1]))
+        self.assertEqual(response.status_code, 200)
 
     def test_no_ressource(self):
         """
         If no ressources exist, an appropriate message is displayed.
         """
+        self.connexion_superuser()
         response = self.client.get(reverse('ressource_management:update',args=[1]))
         self.assertEqual(response.status_code, 404)
 
@@ -64,34 +161,71 @@ class UpdateViewTest(TestCase):
         """
         The ressource update page may display ressource.
         """
+        self.connexion_superuser()
         ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
         response = self.client.get(reverse('ressource_management:update',args=[1]))
         self.assertEqual(response.context['ressource'],ressource)
 
 
-class DeleteViewTest(TestCase):
+class DeleteViewTest(General,TestCase):
 
-    def test_no_ressource(self):
+    def test_not_connected(self):
         """
-        If no ressources exist, an appropriate message is displayed.
-        """
-        response = self.client.get(reverse('ressource_management:delete',args=[1]))
-        self.assertEqual(response.status_code, 404)
-
-    def test_past_ressource(self):
-        """
-        The ressource delete page may display ressource.
+        If a user is not connected, a user is blocked
         """
         ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
         response = self.client.get(reverse('ressource_management:delete',args=[1]))
-        self.assertEqual(response.context['ressource'],ressource)
-
-
-class CreateViewTest(TestCase):
+        self.assertEqual(response.status_code, 403)
     
-    def test_page_exist(self):
+    def test_connected_user(self):
         """
-        The page exist
+        If a user is not superuser , a user is blocked
         """
+        self.connexion_user()
+        ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
+        response = self.client.get(reverse('ressource_management:delete',args=[1]))
+        self.assertEqual(response.status_code, 403)
+
+    def test_connected_superuser(self):
+        """
+        If a user is superuser, he has redirigate and a ressource is deleted
+        """
+        self.connexion_superuser()
+        ressource = Ressource.objects.create(type="amphithéâtre", label="A1", localization="Batiment A", people_capacity=200)
+        response = self.client.get(reverse('ressource_management:delete',args=[1]))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(len(Ressource.objects.filter(id=1)),0)
+
+    def test_no_ressource(self):
+        """
+        If no ressource exist, a user is rediriged 302.
+        """
+        self.connexion_superuser()
+        response = self.client.get(reverse('ressource_management:delete',args=[1]))
+        self.assertEqual(response.status_code, 302)
+
+
+class CreateViewTest(General,TestCase):
+
+    def test_not_connected(self):
+        """
+        If a user is not connected, a user is rediriged
+        """
+        response = self.client.get(reverse('ressource_management:add'))
+        self.assertEqual(response.status_code, 302)
+    
+    def test_connected_user(self):
+        """
+        If a user is not superuser , a user is blocked
+        """
+        self.connexion_user()
+        response = self.client.get(reverse('ressource_management:add'))
+        self.assertEqual(response.status_code, 403)
+
+    def test_connected_superuser(self):
+        """
+        If a user is superuser, he has access to the page
+        """
+        self.connexion_superuser()
         response = self.client.get(reverse('ressource_management:add'))
         self.assertEqual(response.status_code, 200)
--- sharingCloud/ressource_management/urls.py
+++ sharingCloud/ressource_management/urls.py
@@ -4,12 +4,11 @@ from . import views
 
 app_name = 'ressource_management'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('index', views.IndexView.as_view(), name='index'),
     path('index_admin',views.IndexAdminView.as_view(), name='index_admin'),
     path('<int:pk>/read', views.DetailView.as_view(), name='detail'),
     path('add', views.CreateView.as_view(), name='add'),
     path('<int:pk>/update', views.UpdateView.as_view(), name='update'),
-    # path('<int:pk>/delete', views.DeleteView.as_view(), name='delete'),
-    path('<int:ressource_id>/delete', views.deleteRessource, name='maxi'),
-    path('redirect_index',views.redirectIndexView,name="redirect_index")
+    path('<int:ressource_id>/delete', views.deleteRessource, name='delete'),
+    path('',views.redirectIndexView,name="redirect_index")
 ]
